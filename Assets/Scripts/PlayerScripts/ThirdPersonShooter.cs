256cf1c7cfe429fe8b867fb1f51b3a831d208f8f
Sachit Nanajkar <sachitnjk@gmail.com>
2023-03-17 05:24:17 +0530

    changing player forward when aiming and aim walking

--- Assets/Scripts/PlayerScripts/ThirdPersonShooter.cs
+++ Assets/Scripts/PlayerScripts/ThirdPersonShooter.cs
@@ -24,23 +24,35 @@ public class ThirdPersonShooter : MonoBehaviour
 
 	private void Update()
 	{
-		if(_input.aim)
-		{
-			cm_AimVirtualCamera.gameObject.SetActive(true);
-			_TPcontroller.SetSensitivity(aimSensitivity);
-		}
-		else
-		{
-			cm_AimVirtualCamera.gameObject.SetActive(false);
-			_TPcontroller.SetSensitivity(normalSensitivity);
-		}
+		Vector3 mouseWorldPosition = Vector3.zero;
 
 		Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
 		Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
 		if(Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask))
 		{
 			debugTransform.position = raycastHit.point;
+			mouseWorldPosition = raycastHit.point;
 		}
+
+		if(_input.aim)
+		{
+			cm_AimVirtualCamera.gameObject.SetActive(true);
+			_TPcontroller.SetSensitivity(aimSensitivity);
+			_TPcontroller.SetRotateOnMove(false);
+
+			Vector3 worldAimTarget = mouseWorldPosition;
+			worldAimTarget.y = transform.position.y;
+			Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
+
+			transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20f);
+		}
+		else
+		{
+			cm_AimVirtualCamera.gameObject.SetActive(false);
+			_TPcontroller.SetSensitivity(normalSensitivity);
+			_TPcontroller.SetRotateOnMove(true);
+		}
+
 
 	}
 
