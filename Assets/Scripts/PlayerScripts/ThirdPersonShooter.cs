36731236bd945d5f9235204598d49b0aefddb7c6
Sachit Nanajkar <sachitnjk@gmail.com>
2023-03-20 04:38:03 +0530

    added pistol animation and animation change on aim and out of aim

--- Assets/Scripts/PlayerScripts/ThirdPersonShooter.cs
+++ Assets/Scripts/PlayerScripts/ThirdPersonShooter.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TreeEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ThirdPersonShooter : MonoBehaviour
 {
@@ -14,15 +15,22 @@ public class ThirdPersonShooter : MonoBehaviour
 	[SerializeField] private LayerMask aimColliderLayerMask;
 	[SerializeField] private Transform pf_BulletProjectile;	
 	[SerializeField] private Transform bulletSpawnPoint;	
-	//[SerializeField] private Transform debugTransform;
 
 	private ThirdPersonController _TPcontroller;
 	private StarterAssetsInputs _input;
+	private Animator animator;
+
+	private PlayerInput _playerInput;
+	private InputAction shootAction;
 
 	private void Awake()
 	{
 		_input = GetComponent<StarterAssetsInputs>();
 		_TPcontroller = GetComponent<ThirdPersonController>();
+		_playerInput = GetComponent<PlayerInput>();
+		shootAction = _playerInput.actions["Shoot"];
+
+		animator = GetComponent<Animator>();
 	}
 
 	private void Update()
@@ -33,7 +41,6 @@ public class ThirdPersonShooter : MonoBehaviour
 		Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
 		if(Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask))
 		{
-			//debugTransform.position = raycastHit.point;
 			mouseWorldPosition = raycastHit.point;
 		}
 
@@ -42,6 +49,7 @@ public class ThirdPersonShooter : MonoBehaviour
 			cm_AimVirtualCamera.gameObject.SetActive(true);
 			_TPcontroller.SetSensitivity(aimSensitivity);
 			_TPcontroller.SetRotateOnMove(false);
+			animator.SetLayerWeight(1, Mathf.Lerp(animator.GetLayerWeight(1), 1f, Time.deltaTime * 10f));
 
 			Vector3 worldAimTarget = mouseWorldPosition;
 			worldAimTarget.y = transform.position.y;
@@ -54,15 +62,26 @@ public class ThirdPersonShooter : MonoBehaviour
 			cm_AimVirtualCamera.gameObject.SetActive(false);
 			_TPcontroller.SetSensitivity(normalSensitivity);
 			_TPcontroller.SetRotateOnMove(true);
+			animator.SetLayerWeight(1, Mathf.Lerp(animator.GetLayerWeight(1), 0f, Time.deltaTime * 10f));
 		}
 
 		if(_input.shoot)
 		{
-			Vector3 aimDir = (mouseWorldPosition - bulletSpawnPoint.position).normalized;
-			Instantiate(pf_BulletProjectile, bulletSpawnPoint.position, Quaternion.LookRotation(aimDir, Vector3.up));
-			_input.shoot = false;
-		}
+			_TPcontroller.SetRotateOnMove(false);
+			//animator.SetLayerWeight(1, Mathf.Lerp(animator.GetLayerWeight(1), 1f, Time.deltaTime * 10f));
 
+			if(shootAction.WasPressedThisFrame())
+			{
+				Vector3 aimDir = (mouseWorldPosition - bulletSpawnPoint.position).normalized;
+				Instantiate(pf_BulletProjectile, bulletSpawnPoint.position, Quaternion.LookRotation(aimDir, Vector3.up));
+			}
+
+			Vector3 worldAimTarget = mouseWorldPosition;
+			worldAimTarget.y = transform.position.y;
+			Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
+
+			transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20f);
+		}
 	}
 
 }
