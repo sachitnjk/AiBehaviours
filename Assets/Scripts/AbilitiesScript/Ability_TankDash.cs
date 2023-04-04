ec8f0116dc1e5d4a9fa89eaf2dcf3c0be1c7cc86
Sachit Nanajkar <sachitnjk@gmail.com>
2023-04-04 17:46:25 +0530

    tank dash ability script made, To be tested

--- Assets/Scripts/AbilitiesScript/Ability_TankDash.cs
+++ Assets/Scripts/AbilitiesScript/Ability_TankDash.cs
@@ -1,18 +1,57 @@
+using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ability_TankDash : MonoBehaviour
+public class Ability_TankDash : MonoBehaviour, IAbilityController
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] private float tanker_dashDistance = 1f;
+	[SerializeField] private float tanker_dashSpeed = 10f;
+	[SerializeField] private int tanker_DashDamage = 2;
+	[SerializeField] private Vector3 tanker_EntityDamageChecker;
+	[SerializeField] private LayerMask entityLayerMask;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private float tanker_EntityDamageCheckerRadius = 5f;
+
+	public void AbilityUse(Vector3 playerPosition)
+	{
+		StartCoroutine(TankerDashCoroutine(playerPosition));
+		EntityDamageChecker();
+	}
+
+	IEnumerator TankerDashCoroutine(Vector3 playerPosition)
+	{
+		Vector3 dashEndPoint = playerPosition + transform.forward * tanker_dashDistance;
+
+		GetComponent<StarterAssetsInputs>().enabled = false;
+
+		float elapsedCoroutineTime = .8f;
+		while (elapsedCoroutineTime < tanker_dashDistance)
+		{
+			elapsedCoroutineTime += Time.deltaTime;
+			float progress = elapsedCoroutineTime / (tanker_dashDistance / 10);
+			Vector3 newPosition = Vector3.Lerp(playerPosition, dashEndPoint, progress);
+			if (Time.deltaTime > 0)
+			{
+				transform.Translate((newPosition - playerPosition).normalized * tanker_dashDistance * tanker_dashSpeed * Time.deltaTime, Space.World);
+			}
+
+			yield return null;
+		}
+		GetComponent<StarterAssetsInputs>().enabled = true;
+	}
+
+	private void EntityDamageChecker()
+	{
+		Collider[] entityInRange = Physics.OverlapSphere(tanker_EntityDamageChecker, tanker_EntityDamageCheckerRadius, entityLayerMask);
+		if (entityInRange != null && entityInRange.Length > 0)
+		{
+			foreach(Collider enity in entityInRange)
+			{
+				//EnemyDamageTake(tanker_DashDamage);
+				//Debug.Log(enemyHpController.e_CurrentHealth);
+			}
+		}
+	}
+}
 }
